3198da239b03bbcd1d6a55c24fcbda2067df8606
janosoto <alejandro.soto@usach.cl>
2014-12-03 17:43:03 -0300

    Arreglo en creación del fixture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202203956) do
+ActiveRecord::Schema.define(version: 20141202225526) do
 
   create_table "competencia", force: true do |t|
     t.string   "nombre_comp"
@@ -65,7 +65,16 @@ ActiveRecord::Schema.define(version: 20141202203956) do
     t.string   "estado_enc"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "etapa_id"
+    t.integer  "competencia_id"
+    t.integer  "recinto_id"
+    t.integer  "id_local"
+    t.integer  "id_visita"
   end
+
+  add_index "encuentros", ["competencia_id"], name: "index_encuentros_on_competencia_id", using: :btree
+  add_index "encuentros", ["etapa_id"], name: "index_encuentros_on_etapa_id", using: :btree
+  add_index "encuentros", ["recinto_id"], name: "index_encuentros_on_recinto_id", using: :btree
 
   create_table "etapas", force: true do |t|
     t.string   "nombre_etp"
